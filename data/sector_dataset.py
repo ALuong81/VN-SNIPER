692b927f5451591a59d31955e40e5dbb66e8ef36
ALuong81 <anhln81@gmail.com>
2026-03-20 02:08:41 +0700

    Create sector_dataset.py

---
+++ data/sector_dataset.py
@@ -0,0 +1,57 @@
+SECTOR_DATA = {
+    # Ngân hàng
+    "VCB": "Ngân hàng",
+    "BID": "Ngân hàng",
+    "CTG": "Ngân hàng",
+    "TCB": "Ngân hàng",
+    "MBB": "Ngân hàng",
+    "VPB": "Ngân hàng",
+    "ACB": "Ngân hàng",
+    "LPB": "Ngân hàng",
+    "STB": "Ngân hàng",
+    "HDB": "Ngân hàng",
+
+    # Bất động sản
+    "VIC": "Bất động sản",
+    "VHM": "Bất động sản",
+    "NVL": "Bất động sản",
+    "PDR": "Bất động sản",
+    "KDH": "Bất động sản",
+    "HDG": "Bất động sản",
+    "DXG": "Bất động sản",
+
+    # Chứng khoán
+    "SSI": "Chứng khoán",
+    "VND": "Chứng khoán",
+    "HCM": "Chứng khoán",
+    "VCI": "Chứng khoán",
+    "SHS": "Chứng khoán",
+
+    # Thép
+    "HPG": "Thép",
+    "HSG": "Thép",
+    "NKG": "Thép",
+
+    # Dầu khí
+    "GAS": "Dầu khí",
+    "PLX": "Dầu khí",
+    "PVD": "Dầu khí",
+    "PVS": "Dầu khí",
+
+    # Bán lẻ
+    "MWG": "Bán lẻ",
+    "FRT": "Bán lẻ",
+    "PNJ": "Bán lẻ",
+
+    # Điện
+    "POW": "Điện",
+    "NT2": "Điện",
+
+    # Thực phẩm
+    "VNM": "Thực phẩm",
+    "MSN": "Thực phẩm",
+
+    # Xây dựng
+    "CTD": "Xây dựng",
+    "HBC": "Xây dựng",
+}
