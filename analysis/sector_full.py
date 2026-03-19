ccd3575d4abc40f2786b0f5f74ace59765b02c36
ALuong81 <anhln81@gmail.com>
2026-03-19 23:48:17 +0700

    Update sector_full.py

--- analysis/sector_full.py
+++ analysis/sector_full.py
@@ -1,20 +1,7 @@
-def sector_top(stocks):
+from data.symbol_loader import build_symbol_map
 
-    sector_score = {}
+SYMBOL_MAP = build_symbol_map()
 
-    for s in stocks:
-        sector = s.get("sector", "KHÁC")
-
-        score = s.get("meta_score", 0)
-
-        sector_score.setdefault(sector, []).append(score)
-
-    result = []
-
-    for k, v in sector_score.items():
-        avg = sum(v) / len(v)
-        result.append((k, round(avg, 2)))
-
-    result.sort(key=lambda x: x[1], reverse=True)
-
-    return result[:3]
+def detect_sector(symbol):
+    return SYMBOL_MAP.get(symbol, "KHÁC")
+    
