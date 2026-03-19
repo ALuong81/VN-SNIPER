4be991f8ebaea60658ecdaff4706f47e9c3a3399
ALuong81 <anhln81@gmail.com>
2026-03-20 00:01:32 +0700

    Update sector_full.py

--- analysis/sector_full.py
+++ analysis/sector_full.py
@@ -1,7 +1,20 @@
-from data.symbol_loader import build_symbol_map
+import pandas as pd
 
-SYMBOL_MAP = build_symbol_map()
+df = pd.read_csv("data/full_symbols.csv")
+
+sector_map = dict(zip(df["symbol"], df["sector"]))
+
 
 def detect_sector(symbol):
-    return SYMBOL_MAP.get(symbol, "KHÁC")
-    
+    return sector_map.get(symbol, "KHÁC")
+
+
+def sector_top(stocks):
+
+    score_map = {}
+
+    for s in stocks:
+        sec = s.get("sector", "KHÁC")
+        score_map[sec] = score_map.get(sec, 0) + s.get("meta_score", 0)
+
+    return sorted(score_map.items(), key=lambda x: x[1], reverse=True)[:3]
